595c733ad12bd2c7471bfd1649cd9f9a9824934e
Felix Maxwell <felixmaxwell@comcast.net>
2015-03-22 20:14:17 -0400

    Fixed compile script

--- compile.sh
+++ compile.sh
@@ -4,7 +4,11 @@ echo Attempting to compile
 javac -d ./bin/ ./src/*.java
 
 echo Attempting to package as a jar
-jar -cf GPIOLib.jar ./bin/*
+cd bin
+jar -cf GPIOLib.jar *
+cp GPIOLib.jar ..
+rm -f GPIOLib.jar
+cd ..
 
 echo Copying the jar to the test directory
 cp ./GPIOLib.jar ./test/lib/
