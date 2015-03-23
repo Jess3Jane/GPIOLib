1f49fbcea68ac22bf49b9f6cbad15fdddaeb170e
Felix Maxwell <felixmaxwell@comcast.net>
2015-03-22 23:41:08 -0400

    Added print output to MotorTest

--- test/MotorTest.java
+++ test/MotorTest.java
@@ -8,24 +8,28 @@ class MotorTest {
 		GPIOPin a = GPIOLib.allocateGPIO( 22 );
 		GPIOPin b = GPIOLib.allocateGPIO( 21 );
 
+		boolean aValue = true;
+		boolean bValue = false;
+		double pwmValue = 0;
+
 		pwm.setDuty( 0 );
 		a.setValue( true );
 		b.setDirection( GPIOPinDirection.IN );
-		System.out.println( "Starting state: 0 pwm, a on, b off" );
 
 		Scanner s = new Scanner( System.in );
 		String line;
 		while((line = s.nextLine()) != null){
 			String parts[] = line.split(" ");
 			if( parts[0].equals( "quit" ) ){ return; }
-			else if( parts[0].equals( "pwm" ) ){ pwm.setDuty( Double.parseDouble( parts[1] ) ); }
+			else if( parts[0].equals( "pwm" ) ){ pwm.setDuty( (pwmValue = Double.parseDouble( parts[1] )) ); }
 			else if( parts[0].equals( "a" ) ){
-				if( parts[1].equals( "on" ) ){ a.setDirection( GPIOPinDirection.OUT ); a.setValue( true ); }
-				else{ a.setDirection( GPIOPinDirection.IN ); }
+				if( parts[1].equals( "on" ) ){ a.setDirection( GPIOPinDirection.OUT ); a.setValue( true ); aValue = true; }
+				else{ a.setDirection( GPIOPinDirection.IN ); aValue = false; }
 			}else if( parts[0].equals( "b" ) ){
-				if( parts[1].equals( "on" ) ){ b.setDirection( GPIOPinDirection.OUT ); b.setValue( true ); }
-				else{ b.setDirection( GPIOPinDirection.IN ); }
+				if( parts[1].equals( "on" ) ){ b.setDirection( GPIOPinDirection.OUT ); b.setValue( true ); bValue = true; }
+				else{ b.setDirection( GPIOPinDirection.IN ); bValue = false; }
 			}
+			System.out.format( "Current Value: A=%b B=%b PWM=%f", aValue, bValue, pwmValue );
 		}
 	}catch( Exception e ){
 		e.printStackTrace();
