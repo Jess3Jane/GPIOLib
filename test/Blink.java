1312b5e60ed3ee18784ef7761b2b15325df0ed89
Felix Maxwell <felixmaxwell@comcast.net>
2015-03-22 20:32:29 -0400

    Blink multiple times

--- test/Blink.java
+++ test/Blink.java
@@ -4,9 +4,12 @@ public class Blink {
 	public static void main( String[] args ){
 	try{
 		GPIOPin p = GPIOLib.allocateGPIO( 22 );
-		p.setValue(true);
-		Thread.sleep(1000);
-		p.setValue(false);
+		for( int i = 0; i < 10; ++i ){
+			p.setValue(true);
+			Thread.sleep(1000);
+			p.setValue(false);
+			Thread.sleep(1000);
+		}
 	}catch( Exception e ){
 		e.printStackTrace( System.err );
 	}finally{
