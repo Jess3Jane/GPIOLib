6b614801426c95e4981a1314db4505c30b012df9
Felix Maxwell <felixmaxwell@comcast.net>
2015-04-01 09:10:49 -0400

    Changed a pin

--- test/Turn.java
+++ test/Turn.java
@@ -3,7 +3,7 @@ import com.cubethree.GPIOLib.*;
 public class Turn {
 	public static void main( String[] args ){
 	try{
-		Servo s = GPIOLib.allocateServo( 17 );
+		Servo s = GPIOLib.allocateServo( 4 );
 		s.setAngle( -90 );
 		Thread.sleep( 1000 );
 		for( double a = -90; a < 90; a += 5 ){
