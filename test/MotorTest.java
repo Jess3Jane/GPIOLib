3d6c051ee4776854cfa54467928a49bc8b6ee12f
Felix Maxwell <felixmaxwell@comcast.net>
2015-04-08 09:37:59 -0400

    Changed pins back

--- test/MotorTest.java
+++ test/MotorTest.java
@@ -6,7 +6,7 @@ class MotorTest {
 	try{
 		PWMPin pwm = GPIOLib.allocatePWM( 17 );
 		GPIOPin a = GPIOLib.allocateGPIO( 22 );
-		GPIOPin b = GPIOLib.allocateGPIO( 27 );
+		GPIOPin b = GPIOLib.allocateGPIO( 21 );
 
 		boolean aValue = true;
 		boolean bValue = false;
